c8558ba364be8c4578d2d0d22373ff19586f7049
Yong-Soo <soo_0201@naver.com>
2026-02-16 12:54:37 +0900

    1.2.1. Implement priority scheduling

--- threads/thread.c
+++ threads/thread.c
@@ -166,6 +166,16 @@ thread_print_stats (void) {
 			idle_ticks, kernel_ticks, user_ticks);
 }
 
+void
+test_max_priority (void) {
+	if (!list_empty (&ready_list)) {
+		struct thread *highest_ready = list_entry (list_front (&ready_list), struct thread, elem);
+		if (thread_get_priority() < highest_ready->priority) {
+			thread_yield();
+		}
+	}
+}
+
 /* Creates a new kernel thread named NAME with the given initial
    PRIORITY, which executes FUNCTION passing AUX as the argument,
    and adds it to the ready queue.  Returns the thread identifier
@@ -212,6 +222,8 @@ thread_create (const char *name, int priority,
 	/* Add to run queue. */
 	thread_unblock (t);
 
+	test_max_priority();
+
 	return tid;
 }
 
@@ -227,6 +239,18 @@ thread_block (void) {
 	ASSERT (intr_get_level () == INTR_OFF);
 	thread_current ()->status = THREAD_BLOCKED;
 	schedule ();
+}
+
+/* Helper function to sort ready list.
+
+   To ensure that lower priority threads are placed earlier.
+*/
+static bool
+compare_thread_priority (const struct list_elem *a, const struct list_elem *b, void *aux) {
+	struct thread *t_a = list_entry (a, struct thread, elem);
+	struct thread *t_b = list_entry (b, struct thread, elem);
+
+	return t_a->priority > t_b->priority;
 }
 
 /* Transitions a blocked thread T to the ready-to-run state.
@@ -245,7 +269,7 @@ thread_unblock (struct thread *t) {
 
 	old_level = intr_disable ();
 	ASSERT (t->status == THREAD_BLOCKED);
-	list_push_back (&ready_list, &t->elem);
+	list_insert_ordered (&ready_list, &t->elem, compare_thread_priority, NULL);
 	t->status = THREAD_READY;
 	intr_set_level (old_level);
 }
@@ -308,7 +332,7 @@ thread_yield (void) {
 
 	old_level = intr_disable ();
 	if (curr != idle_thread)
-		list_push_back (&ready_list, &curr->elem);
+		list_insert_ordered (&ready_list, &curr->elem, compare_thread_priority, NULL);
 	do_schedule (THREAD_READY);
 	intr_set_level (old_level);
 }
@@ -364,6 +388,8 @@ thread_wake (int64_t ticks) {
 void
 thread_set_priority (int new_priority) {
 	thread_current ()->priority = new_priority;
+
+	test_max_priority();
 }
 
 /* Returns the current thread's priority. */
